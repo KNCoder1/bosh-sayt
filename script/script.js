ff6d6322b34843618a58f64baab5cedea3948863
Nuriddin <95364145+KNCoder1@users.noreply.github.com>
2021-12-23 16:35:01 +0500

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,31 @@
+function langsetActive(){
+    document.getElementById("lang-div").style.display = "block";
+}
+function dark(){
+    document.getElementById("body").style.backgroundColor = "rgb(40, 40, 40)";
+    document.getElementById("h1").style.color = "white";
+    document.getElementById("aside").style.backgroundColor = "rgb(40, 40, 40)";
+    document.getElementById("aside-text").style.backgroundColor = "rgb(40, 40, 40)";
+    document.getElementById("aside-text1").style.backgroundColor = "rgb(40, 40, 40)";
+    document.getElementById("aside-text2").style.backgroundColor = "rgb(40, 40, 40)";
+    document.getElementById("aside-text3").style.backgroundColor = "rgb(40, 40, 40)";
+    document.getElementById("gamedevtext").style.color = "white";
+    document.getElementById("aside-text").style.color = "white";
+    document.getElementById("aside-text1").style.color = "white";
+    document.getElementById("aside-text2").style.color = "white";
+    document.getElementById("aside-text3").style.color = "white";
+}
+function light(){
+    document.getElementById("body").style.backgroundColor = "rgb(235, 235, 235)";
+    document.getElementById("h1").style.color = "black";
+    document.getElementById("aside").style.backgroundColor = "rgb(235, 235, 235)";
+    document.getElementById("aside-text").style.backgroundColor = "rgb(235, 235, 235)";
+    document.getElementById("aside-text1").style.backgroundColor = "rgb(235, 235, 235)";
+    document.getElementById("aside-text2").style.backgroundColor = "rgb(235, 235, 235)";
+    document.getElementById("aside-text3").style.backgroundColor = "rgb(235, 235, 235)";
+    document.getElementById("gamedevtext").style.color = "black";
+    document.getElementById("aside-text").style.color = "black";
+    document.getElementById("aside-text1").style.color = "black";
+    document.getElementById("aside-text2").style.color = "black";
+    document.getElementById("aside-text3").style.color = "black";
+}
